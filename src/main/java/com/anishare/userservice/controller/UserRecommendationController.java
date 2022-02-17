c705602e43e5415ebf2e144bfd4b05623d6e4d8a
cant-code <ayushc13000@gmail.com>
2022-02-17 15:29:49 +0530

    Changes to controller

--- src/main/java/com/anishare/userservice/controller/UserRecommendationController.java
+++ src/main/java/com/anishare/userservice/controller/UserRecommendationController.java
@@ -1,16 +1,20 @@
 package com.anishare.userservice.controller;
 
 import com.anishare.userservice.dto.ResponseDTO;
+import com.anishare.userservice.dto.UserRecommendationsDTO;
 import com.anishare.userservice.service.UserRecommendationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
+@RequestMapping("/users/")
 public class UserRecommendationController {
 
     private final UserRecommendationService userRecommendationService;
@@ -20,7 +24,7 @@ public class UserRecommendationController {
         this.userRecommendationService = userRecommendationService;
     }
 
-    @GetMapping
+    @GetMapping("/getFrom")
     public ResponseEntity<List<ResponseDTO>> getItems(JwtAuthenticationToken jwt) {
         return ResponseEntity.ok(userRecommendationService.findByFrom(jwt.getToken().getTokenValue(), jwt.getName()));
     }
@@ -28,5 +32,23 @@ public class UserRecommendationController {
     @GetMapping("/getTo")
     public ResponseEntity<List<ResponseDTO>> getToItems(JwtAuthenticationToken jwt) {
         return ResponseEntity.ok(userRecommendationService.findByTo(jwt.getToken().getTokenValue(), jwt.getName()));
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<UserRecommendationsDTO> add(@RequestBody UserRecommendationsDTO dto) {
+        return ResponseEntity
+                .created(URI.create(ServletUriComponentsBuilder.fromCurrentRequestUri().toUriString()))
+                .body(userRecommendationService.add(dto));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserRecommendationsDTO> getById(@PathVariable UUID id) {
+        return ResponseEntity.ok(userRecommendationService.findById(id));
+    }
+
+    @DeleteMapping("/{id}/delete")
+    public ResponseEntity<Void> deleteById(@PathVariable UUID id) {
+        userRecommendationService.delete(id);
+        return ResponseEntity.accepted().build();
     }
 }
